d748e4e0cf0fda3bf1ac05d9f2d0f1ecb437e713
RoadtoS7 <kshvv975@gmail.com>
2022-05-26 22:35:54 +0900

    feat: PlayerName으로 공백이 입력되면 nil을 반환하는지 테스트하는 유닛테스트 구현

--- BowlingTest/PlayerNameTest.swift
+++ BowlingTest/PlayerNameTest.swift
@@ -13,5 +13,9 @@ class PlayerNameTest: XCTestCase {
     func test_PlayerName은_알파벳3글자보다_많으면_nil을_반환한다() {
         XCTAssertNil(PlayerName("abcd"))
     }
+    
+    func test_PlayerName으로_공백이입력되면_nil을_반환한다() {
+        XCTAssertNil(PlayerName(" "))
+    }
 }
 
