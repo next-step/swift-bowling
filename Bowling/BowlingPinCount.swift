779e62045abf8de82d046bbd022c2c9d019f4d47
RoadtoS7 <kshvv975@gmail.com>
2022-05-27 23:04:44 +0900

    refactor: BowlingPinCount 리팩터링

--- Bowling/BowlingPinCount.swift
+++ Bowling/BowlingPinCount.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct BowlingPinCount {
-    
+    private let zero = 0
     let value: Int
     
     init?(_ value: Int) {
-        if value < 0 { return nil }
+        if value < zero { return nil }
         self.value = value
     }
 }
