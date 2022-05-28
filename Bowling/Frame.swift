2bf28ef8a8b8d7796404f24683d7bcb7a9d667d9
woosub-lee <woosub.dev@gmail.com>
2022-05-29 03:21:30 +0900

    feat: roll 10 pin at twice is spare

--- Bowling/Frame.swift
+++ Bowling/Frame.swift
@@ -10,9 +10,19 @@ import Foundation
 struct Frame {
     var state: FrameState = .none
     
+    private var fallenPins: Int = 0
+    private var rollCount: Int = 0
+    
     mutating func roll(fallDown pin: Int) {
-        if pin == 10 {
-            state = .strike
+        self.fallenPins += pin
+        self.rollCount += 1
+        switch fallenPins {
+        case 10 where rollCount == 1:
+            self.state = .strike
+        case 10:
+            self.state = .spare
+        default:
+            self.state = .none
         }
     }
 }
