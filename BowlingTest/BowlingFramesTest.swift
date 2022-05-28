841092a7eb03fdb8c196ccf9ff2f1efda9bfbd3a
RoadtoS7 <kshvv975@gmail.com>
2022-05-29 03:53:46 +0900

    feat: BowlingGame의 다음 frame을 반환하는 nextFrame메서드 유닛테스트 구현

---
+++ BowlingTest/BowlingFramesTest.swift
@@ -0,0 +1,23 @@
+//
+//  BowlingFramesTest.swift
+//  BowlingTest
+//
+//  Created by nylah.j on 2022/05/29.
+//
+
+import XCTest
+
+class BowlingFramesTest: XCTestCase {
+
+    func test_nextFrame_마지막Frame이_pinCount가_더필요하면_마지막Frame을_반환한다() {
+        // given
+        let frame = NormalFrame()
+        let bowlingFrames = BowlingFrames(initialFrame: frame)
+        
+        // when
+        let result = bowlingFrames.nextFrame()
+        
+        // then
+        XCTAssertEqual(result?.counts, frame.counts)
+    }
+}
