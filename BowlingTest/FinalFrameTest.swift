0592862116a31f2462ce87b2155cd8e6ab9a1695
RoadtoS7 <kshvv975@gmail.com>
2022-05-28 21:33:24 +0900

    feat: save_2번째카운트의_상태가_strike일경우_3개의_pinCount를_가질수있다 유닛테스트 구현

--- BowlingTest/FinalFrameTest.swift
+++ BowlingTest/FinalFrameTest.swift
@@ -29,4 +29,26 @@ class FinalFrameTest: XCTestCase {
         XCTAssertTrue(frame.counts.contains(secondPinCount))
         XCTAssertFalse(frame.counts.contains(thirdPinCount))
     }
+    
+    func test_save_2번째카운트의_상태가_strike일경우_3개의_pinCount를_가질수있다() {
+        // given
+        let frame = FinalFrame()
+        guard let firstPinCount = PinCount(10),
+              let secondPinCount = PinCount(10),
+              let thirdPinCount = PinCount(3) else {
+                  XCTFail()
+                  return
+              }
+        
+        // when
+        frame.save(pinCount: firstPinCount)
+        frame.save(pinCount: secondPinCount)
+        frame.save(pinCount: thirdPinCount)
+        
+        // then
+        XCTAssertTrue(frame.counts.contains(firstPinCount))
+        XCTAssertTrue(frame.counts.contains(secondPinCount))
+        XCTAssertTrue(frame.counts.contains(thirdPinCount))
+    }
+
 }
