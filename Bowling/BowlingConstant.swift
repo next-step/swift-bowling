e6d940995bc9e33831aebc660c6f26d3d18922d4
RoadtoS7 <kshvv975@gmail.com>
2022-05-28 21:04:14 +0900

    feat: Bowling 관련 상수를 저장하는 BowlingConstant 구현

---
+++ Bowling/BowlingConstant.swift
@@ -0,0 +1,13 @@
+//
+//  BowlingConstant.swift
+//  Bowling
+//
+//  Created by nylah.j on 2022/05/28.
+//
+
+import Foundation
+
+enum BowlingConstant {
+    static let maxFrameCount = 10
+    static let maxCountValue = 10
+}
