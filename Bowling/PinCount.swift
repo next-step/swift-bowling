204ba6c7337c98dc9964e49c66a620f29bbc3b1c
RoadtoS7 <kshvv975@gmail.com>
2022-05-28 20:20:52 +0900

    refactor: zero 상수 대신 Int.zero 사용하도록 수정

--- Bowling/PinCount.swift
+++ Bowling/PinCount.swift
@@ -8,11 +8,10 @@
 import Foundation
 
 struct PinCount: Equatable {
-    private let zero = 0
     let value: Int
     
     init?(_ value: Int) {
-        if value < zero { return nil }
+        if value < Int.zero { return nil }
         self.value = value
     }
 }
