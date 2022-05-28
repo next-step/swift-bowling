98532bb7f392675e8a64ca078cecb94554dd815b
RoadtoS7 <kshvv975@gmail.com>
2022-05-28 21:40:53 +0900

    feat: FinalFrameTest에서 2번재카운트의_상태가_spare일경우_3개의_pinCount를_가질수있다 체크하는 테스트 구현

--- BowlingTest/FinalFrameTest.swift
+++ BowlingTest/FinalFrameTest.swift
@@ -50,5 +50,25 @@ class FinalFrameTest: XCTestCase {
         XCTAssertTrue(frame.counts.contains(secondPinCount))
         XCTAssertTrue(frame.counts.contains(thirdPinCount))
     }
-
+    
+    func test_save_2번재카운트의_상태가_spare일경우_3개의_pinCount를_가질수있다() {
+        // given
+        let frame = FinalFrame()
+        guard let firstPinCount = PinCount(2),
+              let secondPinCount = PinCount(8),
+              let thirdPinCount = PinCount(3) else {
+                  XCTFail()
+                  return
+              }
+        
+        // when
+        frame.save(pinCount: firstPinCount)
+        frame.save(pinCount: secondPinCount)
+        frame.save(pinCount: thirdPinCount)
+        
+        // then
+        XCTAssertTrue(frame.counts.contains(firstPinCount))
+        XCTAssertTrue(frame.counts.contains(secondPinCount))
+        XCTAssertTrue(frame.counts.contains(thirdPinCount))
+    }
 }
