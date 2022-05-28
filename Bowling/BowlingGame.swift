dfce34a4b83fb3a890009b713ab39c2bcb457434
RoadtoS7 <kshvv975@gmail.com>
2022-05-28 15:16:52 +0900

    feat: PlayerName을 프로퍼티로 갖는 BowlingGame 생성
    BowlingGame은 일반적인 볼링 한판을 의미한다.

---
+++ Bowling/BowlingGame.swift
@@ -0,0 +1,17 @@
+//
+//  BowlingGame.swift
+//  Bowling
+//
+//  Created by nylah.j on 2022/05/28.
+//
+
+import Foundation
+
+struct BowlingGame {
+    private let playerName: PlayerName
+
+    init(playerName: PlayerName) {
+        self.playerName = playerName
+    }
+    
+}
