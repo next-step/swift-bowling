df7f17cd5d896641d826b749a2d8147ca7e1d457
RoadtoS7 <kshvv975@gmail.com>
2022-05-30 23:12:48 +0900

    refactor: BowlingGame에서 BowlingGameStateDelegate를 가지고 게임 상태를 외부에 알릴 수 있도록 수정

--- Bowling/BowlingGame.swift
+++ Bowling/BowlingGame.swift
@@ -10,13 +10,17 @@ import Foundation
 class BowlingGame {
     private let playerName: PlayerName
     private let pinCountReader: PinCountReader
+    private let stateDelegate: BowlingGameStateDelegate?
     
     private var frames: BowlingFrames
     private(set) var currentFrame: Frame
     
-    init(playerName: PlayerName, pinCountReader: PinCountReader) {
+    init(playerName: PlayerName,
+         pinCountReader: PinCountReader,
+         stateDelegate: BowlingGameStateDelegate? = nil) {
         self.playerName = playerName
         self.pinCountReader = pinCountReader
+        self.stateDelegate = stateDelegate
         
         let frame = NormalFrame()
         self.frames = BowlingFrames(initialFrame: frame)
@@ -25,12 +29,15 @@ class BowlingGame {
     
     func start() throws -> ScoreBoard {
         while true {
-            let pinCount = pinCountReader.readPinCount()
+            let index = frames.indexOf(frame: currentFrame)!
+            let pinCount = pinCountReader.readPinCount(ofFrameIndex: index)
             try currentFrame.save(pinCount: pinCount)
+            stateDelegate?.afterReceivePinCount(playerName: playerName, scoreBoard: ScoreBoard(frames: frames))
             guard let nextFrame = frames.nextFrame() else {
                 break
             }
             currentFrame = nextFrame
+            
         }
         
         return ScoreBoard(frames: frames)
