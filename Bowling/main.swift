ffd9541ee2668403ec539905608d0b2be734004f
RoadtoS7 <kshvv975@gmail.com>
2022-05-30 23:18:45 +0900

    feat: 사용자로부터 참가자 이름을 받아서 게임을 실행하는 메인 로직 구현

--- Bowling/main.swift
+++ Bowling/main.swift
@@ -6,9 +6,30 @@
 
 import Foundation
 
+struct BowlingPinCountReader: PinCountReader {
+    func readPinCount(ofFrameIndex index: Int) -> PinCount {
+        let pinCountInput = InputView.readPinCount(of: index)
+        return try! PinCountParser.parse(pinCountInput: pinCountInput)
+    }
+}
+
+struct DefaultBowlingGameStateDelegate: BowlingGameStateDelegate {
+    func afterReceivePinCount(playerName: PlayerName, scoreBoard: ScoreBoard) {
+        let format = ScoreBoardFormat(playerName: playerName, bowlingFrames: scoreBoard)
+        OutputView.print(scoreBoard: format.value)
+    }
+}
+
 do {
+    let nameInput = InputView.readPlayerName()
+    let playerName = try PlayerNameParser.parse(nameInput: nameInput)
+    
+    let bowlingGame = BowlingGame(playerName: playerName,
+                                  pinCountReader: BowlingPinCountReader(),
+                                  stateDelegate: DefaultBowlingGameStateDelegate())
+    _ = try bowlingGame.start()
     
 } catch(let error) {
-    
+    OutputView.print(error: error)
 }
 
