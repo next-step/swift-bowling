ced75e61654e27e6e10f00090605ca51a0c9e454
RoadtoS7 <kshvv975@gmail.com>
2022-05-30 21:10:50 +0900

    feat: 문자열을 PinCount로 파싱하는 PinCountParser 테스트 구현

---
+++ Bowling/Parser/PinCountParser.swift
@@ -0,0 +1,8 @@
+//
+//  PinCountParser.swift
+//  Bowling
+//
+//  Created by nylah.j on 2022/05/30.
+//
+
+import Foundation
---
+++ BowlingTest/PinCountParserTest.swift
@@ -0,0 +1,23 @@
+//
+//  PinCountParserTest.swift
+//  BowlingTest
+//
+//  Created by nylah.j on 2022/05/30.
+//
+
+import XCTest
+@testable import Bowling
+
+class PinCountParserTest: XCTestCase {
+
+    func test_parse_문자열을_PinCount인스턴스로_파싱해_반환한다() {
+        // given
+        let input = "0"
+        
+        // when
+        let pinCount = try PinCountParser.parse(pinCountInput: input)
+        
+        // then
+        XCTAssertEqual(pinCount, PinCount(0))
+    }
+}
