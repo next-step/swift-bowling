3fb9327ec22dd85876d5b9a5e668486d3ab25d75
woosub-lee <woosub.dev@gmail.com>
2022-05-31 15:32:21 +0900

    test: final frame description when get bonus

--- BowlingTest/FinalFrameTest.swift
+++ BowlingTest/FinalFrameTest.swift
@@ -30,4 +30,26 @@ class FinalFrameTest: XCTestCase {
         
         XCTAssertEqual(sut.bonusFrames.count, expected)
     }
+    
+    func test_스페어_처리후_마지막_프레임_description() {
+        sut.roll(fallDown: 9)
+        sut.roll(fallDown: 1)
+        
+        sut.bonusRoll(fallDown: 10)
+        
+        let expected: String = "9|/|X"
+        
+        XCTAssertEqual(sut.description, expected)
+    }
+    
+    func test_스트라이크_처리후_마지막_프레임_description() {
+        sut.roll(fallDown: 10)
+        
+        sut.bonusRoll(fallDown: 10)
+        sut.bonusRoll(fallDown: 10)
+        
+        let expected: String = "X|X|X"
+        
+        XCTAssertEqual(sut.description, expected)
+    }
 }
