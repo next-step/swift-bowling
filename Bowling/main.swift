394b0f6dbf0e62aa4ac9b67bd731478f041b3e6c
ITZombietux <zombietux@naver.com>
2022-05-31 19:59:23 +0900

    feat: 메인 함수 수정

--- Bowling/main.swift
+++ Bowling/main.swift
@@ -6,5 +6,23 @@
 
 import Foundation
 
-print("Hello, World!")
+func main() {
+    let rawPlayers = InputView.readPlayer()
+    let playerNames = StringUtility.splitPlayersName(to: rawPlayers)
+    let game = BowlingGame(with: playerNames)
+    
+    for frameNumber in 1..<11 {
+        game.forEachPlayers { player in
+            while !game.isEndFrame(player: player) {
+                game.roll(player: player, falledPins: InputView.readFallenPins(frameNumber: frameNumber))
+                ResultView.printGame(game)
+            }
+            
+            if frameNumber < 11 {
+                player.updateFrame()
+            }
+        }
+    }
+}
 
+main()
