a7eaebeecbed907a9c75e73d6d741c1297b85e2a
woosub-lee <woosub.dev@gmail.com>
2022-05-31 14:57:44 +0900

    test: missing case about bonusFrame

--- BowlingTest/FinalFrameTest.swift
+++ BowlingTest/FinalFrameTest.swift
@@ -14,10 +14,19 @@ class FinalFrameTest: XCTestCase {
         sut = FinalFrame()
     }
     
-    func test_첫_투구에_10핀을_쓰러트리면_보너스_투구를_두번_갖음() {
+    func test_첫_투구에_10핀을_쓰러트리면_보너스_투구를_두번_가짐() {
         sut.roll(fallDown: 10)
         
         let expected: Int = 2
+        
+        XCTAssertEqual(sut.bonusFrames.count, expected)
+    }
+    
+    func test_두번에_걸처_10핀을_쓰러트리면_보너스_투구를_한번_가짐() {
+        sut.roll(fallDown: 9)
+        sut.roll(fallDown: 1)
+        
+        let expected: Int = 1
         
         XCTAssertEqual(sut.bonusFrames.count, expected)
     }
